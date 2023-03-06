3acc751384a114acf3acaca36fb788ec3aedb029
D-Soft-Tech <dynamicsofttechnology@gmail.com>
2023-03-06 18:24:53 +0100

    Added implementation handler for the custom annotation and also added dependencies for retrofit

--- app/src/main/java/com/dsofttech/annotations/data/UserEntity.kt
+++ app/src/main/java/com/dsofttech/annotations/data/UserEntity.kt
@@ -6,4 +6,18 @@ data class UserEntity(
     val name: String,
     @AllowedDateFormat("\\d{4}-\\d{2}-\\d{2}")
     val birthDate: String,
-)
+) {
+    init {
+        val fields = this::class.java.declaredFields
+        fields.forEach { field ->
+            field.annotations.forEach { annotation ->
+                if (annotation == AllowedDateFormat::class.java) {
+                    val regex = (annotation as AllowedDateFormat).regex
+                    if (!regex.toRegex().matches(birthDate)) {
+                        throw IllegalArgumentException("Birth date: $birthDate is not a valid date")
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dsofttech/annotations/data/remote/apis/JsonPlaceHolderApiService.kt
@@ -0,0 +1,4 @@
+package com.dsofttech.annotations.data.remote.apis
+
+interface JsonPlaceHolderApiService {
+}
